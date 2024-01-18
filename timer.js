00caffdba08b6bd5d8acdef81b87d62e11491b0a
AwesomePotato19 <118589158+AwesomePotato19@users.noreply.github.com>
2024-01-18 10:40:49 +0400

    Update timer.js

--- timer.js
+++ timer.js
@@ -64,6 +64,7 @@ document.getElementById('play').onclick = function() {
 	document.getElementById("clock").classList.remove("hide")
 	document.getElementById("play").classList.add("hide")
 	document.getElementById("singsong").play();
+	document.getElementById("singsong").volume = 1;
 	
 	var music = document.getElementById("beep");
 	music.volume = 0.25;
